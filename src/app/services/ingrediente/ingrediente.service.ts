d73eee5390c977acc29e524c91bc8da13c9ccb13
EduSantiagoR <e.santiago.r05@gmail.com>
2024-01-05 12:12:29 -0600

    Ingrediente Form And GetAll

---
+++ src/app/services/ingrediente/ingrediente.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredienteService {
+  private apiUrl = 'http://192.168.0.201';
+
+  constructor(private httpClient: HttpClient){ }
+
+  getAll(): Observable<any>{
+    return this.httpClient.get<any>(`${this.apiUrl}/api/ingrediente/A`);
+  }
+  getById(id: any): Observable<any>{
+    return this.httpClient.get(`${this.apiUrl}/api/ingrediente/${parseInt(id)}`);
+  }
+  add(datos: any): Observable<any>{
+    return this.httpClient.post(`${this.apiUrl}/api/ingrediente`, datos);
+  }
+  update(datos: any): Observable<any>{
+    return this.httpClient.put(`${this.apiUrl}/api/ingrediente`, datos);
+  }
+  delete(id: any): Observable<any>{
+    return this.httpClient.delete(`${this.apiUrl}/api/ingrediente/${parseInt(id)}`);
+  }
+}
